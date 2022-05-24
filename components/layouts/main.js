fff439cfb02fd786c043b378a36c551a46be9cd9
Naffah Abdulla Rasheed <mail@naffah.me>
2022-05-24 23:04:58 +0500

    feat: Add a navbar
    Currently the home nav-item works since there aren't any about and works
    page

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,11 +1,13 @@
 import { Box, Container } from "@chakra-ui/react";
 import Meta from "../meta";
+import Navbar from "../navbar";
 
 export default function MainLayout({ children }) {
   return (
     <>
       <Meta />
       <Container as="main" maxW="container.md" minH="calc(100vh)">
+        <Navbar />
         <Box>{children}</Box>
       </Container>
     </>
---
+++ components/navbar.js
@@ -0,0 +1,28 @@
+import { Flex, Link } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+
+function NavItem({ children, href }) {
+  const router = useRouter();
+  const active = router.asPath === href;
+
+  return (
+    <NextLink href={href} passHref>
+      <Link px={3} py={2} mx={2} style={{ textDecoration: "none" }}>
+        {children}
+      </Link>
+    </NextLink>
+  );
+}
+
+export default function Navbar() {
+  return (
+    <Flex as="nav" py={4} justify="center">
+      <Flex>
+        <NavItem href="/">Home</NavItem>
+        <NavItem href="/about">About</NavItem>
+        <NavItem href="/works">Works</NavItem>
+      </Flex>
+    </Flex>
+  );
+}
