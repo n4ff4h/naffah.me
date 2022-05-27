0e2aef401fdb4327713ddf560e62d50475f837fb
Naffah Abdulla Rasheed <mail@naffah.me>
2022-05-27 15:19:50 +0500

    feat: Change skill icon color to primary on hover

--- components/skill-icon.js
+++ components/skill-icon.js
@@ -1,11 +1,26 @@
 import { Box, Flex, Heading, WrapItem } from "@chakra-ui/react";
+import React from "react";
 
 export default function SkillIcon({ Icon, name }) {
+  const [color, setColor] = React.useState("#CCD1E4");
+
+  function handleMouseEnter() {
+    setColor("#9672fb");
+  }
+
+  function handleMouseLeave() {
+    setColor("#CCD1E4");
+  }
+
   return (
     <WrapItem>
       <Flex direction="column">
-        <Box pb={4}>
-          <Icon size="6em" color="#CCD1E4" />
+        <Box
+          pb={4}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Icon size="6em" color={color} />
         </Box>
         <Heading fontSize="md" textAlign="center" color="#5b6988">
           {name}
