a100759de330dceaa0b5b27cb69d9efc55ad988f
Naffah Abdulla Rasheed <mail@naffah.me>
2022-05-28 15:35:03 +0500

    feat: Remove box shadows from chakra link
    Use focus-visible package and emotion/react to create global css styles to remove outline and box-shadow

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import theme from "../lib/theme";
 import { motion, AnimatePresence } from "framer-motion";
+import "focus-visible/dist/focus-visible";
+import { Global, css } from "@emotion/react";
 
 // font source
 import "@fontsource/inter/300.css";
@@ -8,6 +10,17 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/800.css";
 import MainLayout from "../components/layouts/main";
+
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -18,6 +31,7 @@ const variants = {
 function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
+      <Global styles={GlobalStyles} />
       <MainLayout>
         <AnimatePresence
           exitBeforeEnter
