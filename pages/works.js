3809f309490c7cf300c05a4eb4b6fc57c2fd80b9
Naffah Abdulla Rasheed <mail@naffah.me>
2022-05-28 21:42:02 +0500

    feat: Add coming soon lottie animation to works page

--- pages/works.js
+++ pages/works.js
@@ -1,9 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import animationData from "../public/lotties/rocket.json";
 
 export default function Works() {
   return (
-    <Flex minH="full" py={12} mb="auto">
-      <></>
+    <Flex minH="full" pb={24} mb="auto" justify="center" direction="column">
+      <Player
+        autoplay
+        loop
+        src={animationData}
+        style={{ height: "300px", width: "300px" }}
+      />
+      <Heading fontSize="3xl" color="darkPurple" textAlign="center">
+        Coming Soon!
+      </Heading>
     </Flex>
   );
 }
